8446f01aaf024a9fc26c20d4d6f254094b20ac12
Teo <seminara.teodoro93@gmail.com>
2025-08-28 17:11:38 +0200

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+// Buon lavoro e buon divertimento! 🥷
+
+ 
+for ( let i=1; i<=100; i++ ){
+
+    if ( (i % 3 === 0) && (i % 5 === 0)) {
+
+        console.log( "FizzBuzz");
+
+    } else if (i % 3 === 0) {
+
+        console.log("Fizz");
+        
+    } else if ( i % 5 === 0) {
+
+        console.log( "Buzz");
+        
+    } else {
+        console.log( i );
+
+    }
+    
+}
